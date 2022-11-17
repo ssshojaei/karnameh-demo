0336cca36bbf354ce2ad7e4d92952e2e9189e4e2
Saleh Shojaei <ordinarysaleh@pm.me>
2022-11-17 12:01:36 +0330

    chore: add title prop to header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,34 +1,43 @@
+import Modal from "components/Modal";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { PlusIcon, TriangleIcon } from "../icons";
 import styles from "./styles.module.scss";
 
 type IHeader = {
-  handleCreateAsk: () => void;
+  title: string;
+  isOpen: boolean;
+  handleToggle: () => void;
 };
 
-const Header = ({ handleCreateAsk }: IHeader) => {
+const Header = ({ title, isOpen, handleToggle }: IHeader) => {
   return (
-    <header className={styles.header}>
-      <div className="container flex items-center">
-        <div className={styles.right}>
-          <h1 className={styles.title}>لیست سوالات</h1>
-        </div>
-        <div className={styles.left}>
-          <button onClick={handleCreateAsk} className="btn btnGreen">
-            <PlusIcon />
-            <span>سوال جدید</span>
-          </button>
-          <div className={styles.profile}>
-            <Image src="/images/profile.jpg" alt="profile" height={44} width={44} />
-            <span>زن زندگی آزادی</span>
-            <div className="text-[#9CAEBB]">
-              <TriangleIcon />
+    <>
+      <Modal isOpen={isOpen} onClose={handleToggle} />
+      <header className={styles.header}>
+        <div className="container flex items-center">
+          <div className={styles.right}>
+            <Link href="/">
+              <h1 className={styles.title}>{title}</h1>
+            </Link>
+          </div>
+          <div className={styles.left}>
+            <button onClick={handleToggle} className="btn btnGreen">
+              <PlusIcon />
+              <span>سوال جدید</span>
+            </button>
+            <div className={styles.profile}>
+              <Image src="/images/profile.jpg" alt="profile" height={44} width={44} />
+              <span>زن زندگی آزادی</span>
+              <div className="text-[#9CAEBB]">
+                <TriangleIcon />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
 
